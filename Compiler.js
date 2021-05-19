2fcd7bb472063477396a45a4c3362f7aa18bbbf5
AssassinAguilar <70576469+AssassinAguilar@users.noreply.github.com>
2021-05-19 15:44:51 +0530

    Update Compiler.js

--- Compiler.js
+++ Compiler.js
@@ -11,17 +11,8 @@ sassScript.src = "https://cdnjs.cloudflare.com/ajax/libs/sass.js/0.9.12/sass.syn
 
 document.head.appendChild(sassScript);
 
-const compileSCSS = () => {
-    Sass.compile($("style[type='scss']").innerHTML, res => {
-        let cssStyleTag = document.createElement("style");
-        cssStyleTag.innerHTML = res.text;
-        res.formatted&&console.error(res.formatted);
-        res.text&&document.head.appendChild(cssStyleTag);
-    });
-}
-
-const compileSASS = () => {
-    Sass.compile($("style[type='sass']").innerHTML, res => {
+onload = () => {
+    Sass.compile($("style").innerHTML, res => {
         let cssStyleTag = document.createElement("style");
         cssStyleTag.innerHTML = res.text;
         res.formatted&&console.error(res.formatted);
