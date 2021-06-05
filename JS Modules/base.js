764358344223e97c8c47faf2d3dff3a2b1f89e83
Assassin <70576469+AssassinAguilar@users.noreply.github.com>
2021-06-05 22:20:06 +0530

    Update base.js

--- JS Modules/base.js
+++ JS Modules/base.js
@@ -1,5 +1,5 @@
 const $ = el => document.querySelector(el);
 const $$ = el => document.querySelectorAll(el);
-const rand = val => Math.floor(Math.random() * val);
+const rand = val => Math.floor(Math.random() * (val || 100));
 
 export { $, $$, rand }
