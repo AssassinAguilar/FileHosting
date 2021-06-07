f28f7a4fc0705947e329a4b2bc90e3937ec6be2c
Assassin <70576469+AssassinAguilar@users.noreply.github.com>
2021-06-07 11:06:15 +0530

    Update base.js

--- JS Modules/base.js
+++ JS Modules/base.js
@@ -1,5 +1,18 @@
 const $ = el => document.querySelector(el);
 const $$ = el => document.querySelectorAll(el);
 const rand = val => Math.floor(Math.random() * (val || 100));
+const searchBar = (elContainer, el, self, displayProperty) => {
+    let elementContainer = document.querySelector(elContainer);
+    let element = elementContainer.querySelectorAll(el);
+    let value = self.value.toUpperCase();
+    for(i = 0; i < element.length; i++){
+        if(element[i].textContent.toUpperCase().indexOf(value) > - 1){
+            element[i].style.display = displayProperty || "block";
+        } else {
+            element[i].style.display = "none";
+        }
+    }
+}
 
-export { $, $$, rand }
+export default { $, $$, rand }
+export { searchBar }
